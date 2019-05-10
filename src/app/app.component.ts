ebcbb129463c45400c5f0121af34213976df35fe
itay <itayoded1@gmail.com>
2019-05-10 18:36:24 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, Injector} from '@angular/core';
+import {LazyComponents, AfterViewLoaded} from './lazy-components';
 
+
+@LazyComponents([{url: './lazy/lazy.component#LazyComponent'}])
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewLoaded {
   title = 'ngx-lazy-components';
+
+  constructor(private injector: Injector) {}
+
+  public afterViewLoaded(): void {
+  }
 }
